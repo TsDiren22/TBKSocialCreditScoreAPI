ac6a2af22192ad35e49cd29c7a3219c2d1d20d06
Diren Ozturk <od.ozturk@student.avans.nl>
2023-11-07 14:11:35 +0100

    prisma added

--- server.js
+++ server.js
@@ -1,27 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
-const cors = require('cors'); // Require cors
+const cors = require('cors');
+const { PrismaClient } = require('@prisma/client'); // Import the Prisma client
+
+const prisma = new PrismaClient(); // Create an instance of the Prisma client
+
 const app = express();
 
-app.use(cors()); // Use cors
+app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb', extended: true, parameterLimit: 50000 }));
 
-// API to write data to JSON file
-app.post('/write', (req, res) => {
-    fs.writeFile('data.json', JSON.stringify(req.body, null, 2), (err) => {
-        if (err) throw err;
-        res.send('Data written to file');
-    });
+// API to create a new user
+app.post('/createUser', async (req, res) => {
+    try {
+        const newUser = await prisma.user.create({
+            data: {
+                name: req.body.name,
+                points: req.body.points,
+            },
+        });
+        res.json(newUser);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error creating user' });
+    }
 });
 
-// API to read data from JSON file
-app.get('/read', (req, res) => {
-    fs.readFile('data.json', (err, data) => {
-        if (err) throw err;
-        res.send(JSON.parse(data));
-    });
+// API to retrieve all users
+app.get('/getUsers', async (req, res) => {
+    try {
+        const users = await prisma.user.findMany();
+        res.json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error retrieving users' });
+    }
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
